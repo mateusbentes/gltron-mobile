cb23a6ebe5d915ec04d8d9949445099f97515d0d
Mateus Sales Bentes <mateusbentes@gmail.com>
2026-03-10 14:23:54 -0300

    Add Android Studio and Xcode stubs; remove build scripts

---
+++ android-studio/app/src/main/java/gltron/mobile/MainActivity.java
@@ -0,0 +1,12 @@
+package gltron.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Placeholder: MonoGame Android bootstrap should be wired here.
+    }
+}
